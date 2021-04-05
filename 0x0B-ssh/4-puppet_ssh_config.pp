523e3295e3518f7e927f18e987a1d30088aa0bdc
David Alzate <illker@hotmail.com>
2021-04-05 15:32:40 +0000

    master of puppets 2 :smirk_cat:

---
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+#  Client configuration file Puppet
+include stdlib
+
+file_line { 'Turn off passwd auth':
+  path => '/etc/ssh/ssh_config'
+  line => 'IdentityFile ~/.ssh/holberton',
+}
+
+file_line { 'Declare identity file':
+  path => '/etc/ssh/ssh_config'
+  line => 'PasswordAuthentication no',
+}
