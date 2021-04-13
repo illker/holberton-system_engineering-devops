5e5dcdf4ab9063bee90cc84783f7e92e18719a13
illker <illker@hotmail.com>
2021-04-12 22:48:56 -0500

    puppets :snake::elephant:

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,10 +1,5 @@
 # Add a custom HTTP header with Puppet
 
 exec { 'eNginx':
-    command  =>'apt-get -y update && sudo apt-get -y upgrade && apt-get -y install nginx
-    && ufw allow 'Nginx HTTP' && "Holberton School" > /var/www/html/index.nginx-debian.html
-    && sed -i "/server_name _;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default
-    && service nginx restart',
+    command  =>'apt-get -y update && sudo apt-get -y upgrade && apt-get -y install nginx && ufw allow "Nginx HTTP" && "Holberton School" > /var/www/html/index.nginx-debian.html && sed -i "/server_name _;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default && service nginx restart',
 }
-
-
