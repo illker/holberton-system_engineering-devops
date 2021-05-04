339d925c26d8246a998272e8b36874a6e715a2a5
illker <illker@hotmail.com>
2021-05-03 23:58:37 -0500

    json dic :floppy_disk:

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -7,22 +7,23 @@ from sys import argv
 
 if __name__ == '__main__':
 
-    api_url = 'https://jsonplaceholder.typicode.com/users/'
-    r = requests.get(api_url).json()
-    dic_id = {}
-    for bu in r:
-        dic_id[bu.get('id')] = bu.get('username')
-    r2 = requests.get('https://jsonplaceholder.typicode.com/todos').json()
-    dic_f = {}
-    for bu in r2:
-        user_id = bu.get("userId")
-        username = dic_id[user_id]
-        title = bu.get("title")
-        completed = bu.get("completed")
-        dic = {"username": username, "task": title, "completed": completed}
+    req = requests.get('https://jsonplaceholder.typicode.com/users/')
+    req = req.json()
+    ids = {}
+    for user in req:
+        ids[user.get("id")] = user.get("username")
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos').json()
+    dic = {}
+    for task in todos:
+        user_id = task.get("userId")
+        username = ids[user_id]
+        t = task.get("title")
+        completed = task.get("completed")
+        d = {"username": username, "task": t, "completed": completed}
         if dic.get(user_id):
-            dic.get(user_id).append(dic)
+            dic.get(user_id).append(d)
         else:
-            dic_f[user_id] = [dic]
-    with open('todo_all_employees.json', 'w', newline='') as burger:
-        json.dump(dic_f, burger)
+            dic[user_id] = [d]
+
+    with open('todo_all_employees.json', 'w', newline='') as f:
+        json.dump(dic, f)
