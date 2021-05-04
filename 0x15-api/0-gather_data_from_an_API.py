7cd0ba93e35fdb3cfc8453f00c056e46ea13d0aa
illker <illker@hotmail.com>
2021-05-03 19:29:12 -0500

    get the api :imp:

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Gather data from an API"""
+
+import requests
+from sys import argv
+
+
+if __name__ == '__main__':
+
+    em_id = argv[1]
+    api_url = 'https://jsonplaceholder.typicode.com/users/' + em_id
+    r = requests.get(api_url)
+    name = r.json().get('name')
+    url = 'https://jsonplaceholder.typicode.com/users/' + em_id + '/todos'
+    r2 = requests.get(url).json()
+    burger = [bu for bu in r2 if bu['completed'] is True]
+
+    print("Employee {} is done with tasks({}/{}):"
+          .format(name, len(burger), len(r2)))
+
+    for beer in burger:
+        print('\t ' + beer['title'])
