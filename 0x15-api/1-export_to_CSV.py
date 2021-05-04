fe2de9331352ed6c4b34e05e855b093e0cc0e77a
illker <illker@hotmail.com>
2021-05-03 21:02:31 -0500

    csv :zap:

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Gather data from an API"""
+
+import requests
+from sys import argv
+import csv
+
+
+if __name__ == '__main__':
+    em_id = argv[1]
+    api_url = 'https://jsonplaceholder.typicode.com/users/' + em_id
+    r = requests.get(api_url)
+    username = r.json().get('username')
+
+    url = 'https://jsonplaceholder.typicode.com/users/' + em_id + '/todos'
+    r2 = requests.get(url).json()
+
+    file = em_id + '.csv'
+    with open(file, 'w', newline='') as bu:
+        burger = csv.writer(bu, quoting=csv.QUOTE_ALL)
+        [burger.writerow([em_id, username, beer.get(
+            'completed'), beer.get('title')]) for beer in r2]
