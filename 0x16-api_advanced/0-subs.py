383009d7830d01ae66aad7d9206343a0fa52012e
illker <illker@hotmail.com>
2021-05-17 16:31:29 -0500

    subreddits :speech_balloon:

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,8 +8,8 @@ import requests
 def number_of_subscribers(subreddit):
     """How many subs?"""
     burger = 'https://www.reddit.com/r/' + subreddit + '/about.json'
-    headers = {'User-Agent': 'linux: burger:v1.0.0 (by /u/da2a)'}
-    request = requests.get(burger, headers)
+    h = {'User-Agent': 'linux: burger:v1.0.0 (by /u/da2a)'}
+    request = requests.get(burger, headers=h)
     if request.status_code == 200:
         return request.json().get('data').get('subscribers')
     else:
